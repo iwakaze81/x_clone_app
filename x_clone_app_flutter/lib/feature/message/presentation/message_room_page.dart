331e332c077f133a8166d3110eb3b0bc6264034a
iwakaze81 <iwakaze81@gmail.com>
2024-03-14 21:38:16 +0900

    Remove unused local variable

--- x_clone_app_flutter/lib/feature/message/presentation/message_room_page.dart
+++ x_clone_app_flutter/lib/feature/message/presentation/message_room_page.dart
@@ -2,7 +2,6 @@ import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:serverpod_auth_shared_flutter/serverpod_auth_shared_flutter.dart';
 import 'package:serverpod_chat_flutter/serverpod_chat_flutter.dart';
 import 'package:x_clone_app_flutter/feature/util/singletones.dart';
 import 'package:x_clone_app_flutter/serverpod/api_client.dart';
@@ -18,9 +17,6 @@ class MessageRoomPage extends HookConsumerWidget {
     final controller = useState<ChatController?>(null);
     useEffect(() {
       singleton<ApiClient>().client.openStreamingConnection().then((_) {
-        final connectStatus =
-            singleton<ApiClient>().client.streamingConnectionStatus;
-
         final chatController = ChatController(
           channel: roomId.toString(),
           module: singleton<ApiClient>().client.modules.chat,
